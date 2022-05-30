348269d84ddbafabe97f24b699dad11e9b49fc96
sebavazque <sebavqzquez00@gmail.com>
2022-05-30 20:42:56 -0300

    Suim

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './Componentes/Header';
+import All from './Componentes/All';
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './Componentes/Footer';
+import Proyectos from './Componentes/Proyectos';
+import Contacto from './Componentes/Contacto';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+          <Route path="/inicio" element={<All />} />
+          <Route path="/proyectos" element={<Proyectos />} />
+          <Route path="/contacto" element={<Contacto/>} />
+      </Routes>  
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
